18eadf12064ca437d3d2d69dfb96ed96076b108d
papavask <44700690+papavask@users.noreply.github.com>
2025-02-21 00:19:35 +0200

    Update butt01.py

--- Source/butt01.py
+++ Source/butt01.py
@@ -95,7 +95,7 @@ def start_main():
         st.session_state.Selected_station = ""
 
     if 'stop_processes1' not in st.session_state:
-    st.session_state.stop_processes1 = False
+        st.session_state.stop_processes1 = False
 
     file_path = "./Source/RadioList.csv"
     data = pd.read_csv(file_path, sep=",")
