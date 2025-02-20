6226f648fc49d6132a7a46b4d8e98c5c0d31f935
papavask <44700690+papavask@users.noreply.github.com>
2025-02-20 23:11:40 +0200

    Update butt01.py

--- Source/butt01.py
+++ Source/butt01.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import logging
 import pandas as pd
+import asyncio
 
 
 
@@ -28,7 +29,10 @@ def click_btn01(Radio_url):
     st.session_state.Button01_clicked = True
     st.session_state.Btn01_Dis = True
     st.session_state.Btn02_Dis = False
-    st.audio(Radio_url, format="audio/mp3", autoplay=True)
+    await play(URL_Station)
+
+async def play(Station_url):
+    st.audio(Station_url, format="audio/mp3", autoplay=True)
 
 
 def click_btn02():
